199af7b447e3689bb25e2962f4c5536e7391f298
Momoka <one.momo421@gmail.com>
2024-12-28 11:18:32 +1300

    initial commit
    inital commit

--- src/apiKeys.js
+++ src/apiKeys.js
@@ -1,5 +1,5 @@
 // Visit https://api.openweathermap.org & then signup to get our API keys for free
 module.exports = {
-  key: "{Your API Key Here}",
+  key: "a40a5ff3e3416873969d209ca3d61c8e",
   base: "https://api.openweathermap.org/data/2.5/",
 };
